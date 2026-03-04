212a2ad205b1be9c48db9302942183b4327e5cda
emkumthekar <emk28470@uga.edu>
2026-03-03 19:33:30 -0500

    db changes

--- BackEndDev/theatreBooking/src/main/java/theatreBooking/MovieController.java
+++ BackEndDev/theatreBooking/src/main/java/theatreBooking/MovieController.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/movies")
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:4200")
 public class MovieController {
 
     @Autowired
