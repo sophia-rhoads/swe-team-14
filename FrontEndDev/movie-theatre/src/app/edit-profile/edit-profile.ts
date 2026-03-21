658c4c227b2c4a1d70827d5288d953291e913c8c
sophia-rhoads <sdr92017@uga.edu>
2026-03-20 21:33:25 -0400

    Basic outline for new frontend features

---
+++ FrontEndDev/movie-theatre/src/app/edit-profile/edit-profile.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-edit-profile',
+  imports: [CommonModule],
+  templateUrl: './edit-profile.html',
+  styleUrl: './edit-profile.scss',
+  standalone: true
+})
+export class EditProfile {
+
+}
