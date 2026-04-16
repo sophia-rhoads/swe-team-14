3857edbe0f741d5529bd5f585e648c959c33c46d
CharikCh-24 <235216102+CharikCh-24@users.noreply.github.com>
2026-04-16 15:16:59 -0400

    Demo version
    Pushing the rest of the codes

--- BackEndDev/theatreBooking/src/main/java/theatreBooking/BookingHistoryRepo.java
+++
@@ -1,9 +0,0 @@
-package theatreBooking;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface BookingHistoryRepo extends JpaRepository<BookingHistory, Long> {
-    List<BookingHistory> findByCustomerIdOrderByBookedAtDesc(Long customerId);
-}
